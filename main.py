fa723a9bef11cb9cd84d8d0589ff88ec801f400d
Olivia12345 <oliviaonu@outlook.com>
2023-09-05 23:02:43 -0700

    BIP CC Calculator

--- main.py
+++ main.py
@@ -1,41 +1,45 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
-# Get the transaction date from the user
-transaction_date_str = input("Enter the transaction date (YYYY-MM-DD): ")
+while True:
+    # Get the transaction date from the user
+    transaction_date_str = input("Enter the transaction date (YYYY-MM-DD), or type 'done' to exit: ")
 
-try:
-    # Convert the transaction date string to a datetime object
-    transaction_date = datetime.strptime(transaction_date_str, "%Y-%m-%d")
+    if transaction_date_str.lower() == "done":
+        break
 
-    # Get the current date
-    current_date = datetime.now()
+    try:
+        # Convert the transaction date string to a datetime object
+        transaction_date = datetime.strptime(transaction_date_str, "%Y-%m-%d")
 
-    # Check if the year entered by the user is the same as the year in the current date
-    if transaction_date.year != current_date.year:
-        print("Please enter the current year.")
-    else:
-        # Calculate the payment due date
-        if transaction_date.day < 18:
-            # Transaction date is before the 18th, payment due date is the 4th of the next month
-            if transaction_date.month == 12:
-                due_date = datetime(transaction_date.year + 1, 1, 4)
-            else:
-                due_date = datetime(transaction_date.year, transaction_date.month + 1, 4)
+        # Get the current date
+        current_date = datetime.now()
+
+        # Check if the year entered by the user is the same as the current year
+        if transaction_date.year != current_date.year:
+            print("Please enter the current year.")
         else:
-            # Transaction date is on or after the 18th, payment due date is the 4th of the month after the next
-            if transaction_date.month == 11:
-                due_date = datetime(transaction_date.year + 1, 1, 4)
-            elif transaction_date.month == 12:
-                due_date = datetime(transaction_date.year + 1, 2, 4)
+            # Calculate the payment due date
+            if transaction_date.day < 18:
+                # Transaction date is before the 18th, payment due date is the 4th of the next month
+                if transaction_date.month == 12:
+                    due_date = datetime(transaction_date.year + 1, 1, 4)
+                else:
+                    due_date = datetime(transaction_date.year, transaction_date.month + 1, 4)
             else:
-                due_date = datetime(transaction_date.year, transaction_date.month + 2, 4)
+                # Transaction date is on or after the 18th, payment due date is the 4th of the month after the next
+                if transaction_date.month == 11:
+                    due_date = datetime(transaction_date.year + 1, 1, 4)
+                elif transaction_date.month == 12:
+                    due_date = datetime(transaction_date.year + 1, 2, 4)
+                else:
+                    due_date = datetime(transaction_date.year, transaction_date.month + 2, 4)
 
-        # Calculate the number of days until due date
-        days_until_due = (due_date - current_date).days
+            # Calculate the number of days until the due date
+            days_until_due = (due_date - current_date).days
 
-        # Print the payment due date and number of days until due
-        print("Payment due date:", due_date.strftime("%Y-%m-%d"))
-        print("Days until due:", days_until_due)
-except ValueError:
-    print("Invalid date format. Please enter the date in the format YYYY-MM-DD.")
+            # Print the payment due date and the number of days until due
+            print("Payment due date:", due_date.strftime("%Y-%m-%d"))
+            print("Days until due:", days_until_due)
 
+    except ValueError:
+        print("Invalid date format. Please enter the date in the format YYYY-MM-DD.")
